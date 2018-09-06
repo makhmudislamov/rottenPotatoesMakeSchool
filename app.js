87fd3f213654fd73e67451e174605ad9d6ee400c
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-09-05 20:16:05 -0700

    Users can create reviews

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
 
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+// mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', {
+  useMongoClient: true
+});
+
+// Review model. declaration is similari to class. Classes are saved in recent memory and models are on the database.
+const Review = mongoose.model('Review', {
+  title: String,
+  description: String,
+  movieTitle: String
+});
+
 // handlebar
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -22,26 +50,16 @@ app.set('view engine', 'handlebars');
 //   res.render('reviews-index', { reviews: reviews });
 // })
 
-// updating root and using Review model frmo DB
-app.get('/', (req, res) => {
+// updating root and using Review model from DB
+app.get('/reviews', (req, res) => {
   Review.find()
     .then(reviews => {
-      res.render('reviews-index', { reviews: reviews });
+      res.render('reviews-new', { reviews: reviews });
     })
     .catch(err => {
       console.log(err);
     })
-}
-
-// mongoose
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
-
-// Review model. declaration is similari to class. Classes are saved in recent memory and models are on the database.
-const Review = mongoose.model('Review', {
-  title: String
-});
-
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
