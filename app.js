dbadf72533fdff9651dbe390543a464707d167ab
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-09-05 21:41:08 -0700

    edit/update function

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 var exphbs = require('express-handlebars');
+
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// CREATE
-app.post('/reviews', (req, res) => {
-  Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
 
 // mongoose
 const mongoose = require('mongoose');
@@ -46,7 +41,17 @@ app.get('/', (req, res) => {
     })
 })
 
-// Route for creating new review
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+// NEW
 app.get('/reviews/new', (req, res) => {
   Review.find()
     .then(reviews => {
@@ -57,13 +62,31 @@ app.get('/reviews/new', (req, res) => {
     })
 })
 
-// Showing old reviews
+// SHOW
 app.get('/reviews/:id', (req, res) => {
   Review.findById(req.params.id).then((review) => {
     res.render('reviews-show', { review: review })
   }).catch((err) => {
     console.log(err.message);
   })
+})
+
+// EDIT
+app.get('/reviews/:id/edit', function (req, res) {
+  Review.findById(req.params.id, function(err, review) {
+    res.render('reviews-edit', {review: review});
+  })
+})
+
+// UPDATE
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
 })
 
 app.listen(3000, () => {
