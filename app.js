f89f7bc2c1c776e7165a526ab3eb790e544db474
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-09-05 21:48:49 -0700

    Users can destroy reviews

--- app.js
+++ app.js
@@ -89,6 +89,16 @@ app.put('/reviews/:id', (req, res) => {
     })
 })
 
+// DELETE
+app.delete('/reviews/:id', function (req, res) {
+  console.log("DELETE review")
+  Review.findByIdAndRemove(req.params.id).then((review) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
